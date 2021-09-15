ef48944021147e96af6d197ecce78508a8a54293
RenanLBastos <renanlbastos@hotmail.com>
2021-09-15 04:59:48 -0300

    Mudaça de double para Number

--- src/main/java/com/example/Calculadora/Calculadora.java
+++ src/main/java/com/example/Calculadora/Calculadora.java
@@ -3,6 +3,7 @@ package com.example.Calculadora;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Calculadora {
 
@@ -12,7 +13,7 @@ public class Calculadora {
         }
 
         @GetMapping("api/v1/calcular/sum")
-        public double sum(double a, double b) {
+        public Number sum(double a, double b) {
             double sum = a + b;
             if ((sum == Math.floor(sum)) &&!Double.isInfinite(sum)) {
                 return (int) sum;
@@ -23,7 +24,7 @@ public class Calculadora {
         }
 
         @GetMapping("api/v1/calcular/subtract")
-        public double subtract(double a, double b) {
+        public Number subtract( double a,  double b) {
             double subtract = a - b;
             if ((subtract == Math.floor(subtract)) &&!Double.isInfinite(subtract)) {
                 return (int) subtract;
@@ -33,7 +34,7 @@ public class Calculadora {
         }
 
         @GetMapping("api/v1/calcular/multply")
-        public double multiply(double a, double b) {
+        public Number multiply(double a, double b) {
             double multiplicy = a * b;
             if ((multiplicy == Math.floor(multiplicy)) &&!Double.isInfinite(multiplicy)) {
                 return (int) multiplicy;
@@ -43,7 +44,7 @@ public class Calculadora {
         }
 
         @GetMapping("api/v1/calcular/divide")
-        public double divide(double a, double b) {
+        public Number divide(double a, double b) {
             double divide = a / b;
             if ((divide == Math.floor(divide)) &&!Double.isInfinite(divide)) {
                 return (int) divide;
@@ -53,7 +54,7 @@ public class Calculadora {
         }
 
         @GetMapping("api/v1/calcular/module")
-        public double module(double a, double b) {
+        public Number module(double a, double b) {
             double module = a % b;
             if ((module == Math.floor(module)) &&!Double.isInfinite(module)) {
                 return (int) module;
